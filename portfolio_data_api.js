59c573e369d900951f09d4b5ddd9dafa72cbe5a0
kholilibrahim4 <kholilibrahim4@gmail.com>
2024-12-18 00:39:32 +0400

    Create portfolio_data_api.js

---
+++ portfolio_data_api.js
@@ -0,0 +1,77 @@
+const filterData = [
+    {
+        categories: 'web bootstrap',
+        portfolioImg: { portImg24 },
+        title: 'Mehedee Hasan Portfolio',
+        tagline: 'Date: 14 July, 2023',
+        hrefLink: 'https://hasanrony364.github.io/portfolio/',
+        projectBgNumber: `${styles.projectBg24}`
+    },
+
+    {
+        categories: 'web',
+        portfolioImg: { portImg1 },
+        title: 'Jean Cote',
+        tagline: 'The Business Automation Architec',
+        hrefLink: 'https://www.jeancotemarketing.com/',
+        projectBgNumber: `${styles.projectBg1}`
+    },
+    {
+        categories: 'web',
+        portfolioImg: { portImg2 },
+        title: 'Eeasy Rooms',
+        tagline: 'Online Hotels',
+        hrefLink: '#',
+        projectBgNumber: `${styles.projectBg2}`
+    },
+    {
+        categories: 'web bootstrap codepen',
+        portfolioImg: { portImg25 },
+        title: 'Bootstrap Tab',
+        tagline: 'Singin & Signup by bootstrap tab',
+        hrefLink: 'https://codepen.io/mdnayan/pen/vZbZMw',
+        projectBgNumber: `${styles.projectBg25}`
+    },
+    {
+        categories: 'web',
+        portfolioImg: { portImg3 },
+        title: 'Green Design',
+        tagline: 'Innovative & Sustainable Solutions',
+        hrefLink: 'http://www.greendesign.ca/',
+        projectBgNumber: `${styles.projectBg3}`
+    },
+    {
+        categories: 'web',
+        portfolioImg: { portImg4 },
+        title: 'We Monitor Email',
+        tagline: 'Solve your email problems',
+        hrefLink: 'https://www.wemonitoremail.com/',
+        projectBgNumber: `${styles.projectBg4}`
+    },
+    {
+        categories: 'codepen',
+        portfolioImg: { portImg8 },
+        title: 'Codepen',
+        tagline: 'My Codepen Works',
+        hrefLink: 'https://codepen.io/mdnayan',
+        projectBgNumber: `${styles.projectBg8}`
+    },
+    {
+        categories: 'web',
+        portfolioImg: { portImg5 },
+        title: 'Ipac',
+        tagline: 'Western Austrakia',
+        hrefLink: 'http://ipacwa.com.au/',
+        projectBgNumber: `${styles.projectBg5}`
+    },
+    {
+        categories: 'web',
+        portfolioImg: { portImg6 },
+        title: 'Jenna Forster',
+        tagline: 'Master The Inner Game Of Happiness',
+        hrefLink: 'http://kennethpugmire.com/jenna/',
+        projectBgNumber: `${styles.projectBg6}`
+    },
+]
+
+
